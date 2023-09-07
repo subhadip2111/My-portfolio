37d54045b659cd9441ca8b6e03cba3bfad05b89a
subhadipshee <subhadipshee20010521@gmail.com>
2023-09-07 20:43:30 +0530

    done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -81,7 +81,7 @@ const Intro = () => {
 
       <img
         src="https://images.unsplash.com/photo-1518530179311-641ebb3b6b1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-        alt="Profile"
+        alt="Profile" className="images"
       />
     </section>
   );
