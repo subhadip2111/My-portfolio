7b202ed9f10baba985d25ee7498d4fd9d7a7408d
subhadipshee <subhadipshee20010521@gmail.com>
2023-09-07 20:07:52 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import "./navbar.css";
 import contactImg from "../../assets/contact.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
@@ -27,16 +35,36 @@ const Navbar = () => {
             Contact ME
           </button>
         </Link>
-        
       </div>
+
+      <div
+        className={`mobileMenuIcon ${mobileMenuOpen ? "mobileMenuActive" : ""}`}
+        onClick={toggleMobileMenu}
+      >
+        <FontAwesomeIcon icon={mobileMenuOpen ? faTimes : faBars} />
+      </div>
+
+      <ul
+        className={`mobileMenuList ${mobileMenuOpen ? "mobileMenuActive" : ""}`}
+      >
+        <li className="mobileMenuListItem">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="mobileMenuListItem">
+          <Link to="/skills">About</Link>
+        </li>
+        <li className="mobileMenuListItem">
+          <Link to="/works">Portfolio</Link>
+        </li>
+        <li className="mobileMenuListItem">
+          <Link to="/project">Projects</Link>
+        </li>
+        <li className="mobileMenuListItem">
+          <Link to="/contact">Contact ME</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 export default Navbar;
-
-
-
-
-
-
